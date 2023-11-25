e531d0250b2d70a3f1c704235e68b13c8fc591ff
Martin Cho <martinizer90@gmail.com>
2023-11-25 00:51:13 -0600

    Editor scripting worked
    It worked when starting the new unity project. Problem seems to be solved for now.

--- Assets/Scenes/PlayerSHip/Editor/AsteroidEditor.cs
+++ Assets/Scenes/PlayerSHip/Editor/AsteroidEditor.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 
 
-namespace CustomEditor
+public class AsteroidEditor : EditorWindow
 {
-    public class AsteroidEditor : EditorWindow
+    [MenuItem("Assets/Asteroid Editor Tool")]
+    public static void ShowWindow()
     {
-        [MenuItem("[CustomEditor]/Asteroid Editor Tool")]
-        public static void ShowWindow()
-        {
-            EditorWindow.GetWindow<AsteroidEditor>("Asteroid Editor Tool");
-        }
+        EditorWindow.GetWindow<AsteroidEditor>("Asteroid Editor Tool");
+    }
 
-        private void OnGUI()
-        {
-            //window code goes here
-            GUILayout.Label("Selection Filters: ", EditorStyles.boldLabel);
-        }
+    private void OnGUI()
+    {
+        //window code goes here
+        GUILayout.Label("Selection Filters: ", EditorStyles.boldLabel);
     }
 }
